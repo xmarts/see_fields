f0885332214eddc19924e74f0a7ada57b4f9eee9
Nay3li <nayeli.valencia@xmarts.com.mx>
2018-01-25 15:43:52 -0600

    Ver campos de productos ocultos por variante

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'Hidde fields product',
+    'summary': 'Ver campos de productos ocultos por variante',
+
+    'description': """
+    Ver campos de productos ocultos por variante
+    """,
+    'author': "Xmarts, Nayeli Valencia Díaz",
+    'website': "http://www.xmarts.com",
+    'depends': ['base'],
+    'data': [
+        'views/views.xml',
+    ]
+}
---
+++ models/models.py
@@ -0,0 +1,12 @@
+## -*- coding: utf-8 -*-
+
+from openerp import models, fields, api, _, tools
+from openerp.exceptions import UserError, RedirectWarning, ValidationError
+import xlrd
+import shutil
+import logging
+_logger = logging.getLogger(__name__)
+
+#class xmartsstockpicking(models.Model):
+#	_inherit ='stock.picking'
+#  	xmpack= fields.Boolean(string="Empaquetado",default=False)
